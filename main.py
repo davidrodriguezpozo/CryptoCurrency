7cbc84d911b5d455f90cfc1e7d767dd39926c85a
David Rodriguez <davidrp1996@gmail.com>
2021-09-04 17:19:20 +0200

    Fixes in main.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import tornado
 from miner import Miner
 from node import Node
 from connections import run_server, remote_connection
+from secret import SERVER_ADDRESS
 
 MINER_ADDRESS = 'db82f482cdf4922fa2c957dc7c997c48bf0c12ba'
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         REMOTE_NODES = [
-            "ws://ec2-18-135-206-224.eu-west-2.compute.amazonaws.com:46030/"]
+            SERVER_ADDRESS]
         print('Starting sqlite DB...')
         node = Node.start("./blocks.sqlite").proxy()
         print('Node started')
